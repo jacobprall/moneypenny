99bf46806f74d428af3764abfd6676d0c416345a
Jacob <prall.jacob@gmail.com>
2020-08-27 10:46:58 -0700

    updated api

--- frontend/util/news_util.js
+++ frontend/util/news_util.js
@@ -1,7 +1,7 @@
 export default function fetchBusinessNews() {
   return (
   $.ajax({
-    url: 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c52b1b6b03304f4b89a6fbfc26a4b2d5'
+    url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c52b1b6b03304f4b89a6fbfc26a4b2d5'
   })
   )
-}
+};
